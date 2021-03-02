bde69004b9dfa36a6c93f07e63b1ec5a739ba1e7
Paul_Laptop <pjansen487@gmail.com>
2021-03-02 10:07:44 +0100

    Personenverwaltung

---
+++ Personenverwaltung/Controllers/PersonController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MySql.Data.MySqlClient;
+using Personenverwaltung.Models;
+
+namespace Personenverwaltung.Controllers
+{
+    public class PersonController : ApiController
+    {
+        // GET: api/Person
+        public List<Person> Get()
+        {
+            List<Person> ListPS = new List<Person>();
+            List<Mannschaft> ListMS = new List<Mannschaft>();
+            string cnst = "Server=localhost;Database=microservicespro;Uid=root;";
+            MySqlConnection con = new MySqlConnection(cnst);
+            try
+            {
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from Fussballspieler", con);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    ListPS.Add(new Fussballspieler(reader.GetString("Name"), reader.GetInt32("Tore"), reader.GetInt32("Siege"), reader.GetInt32("Nummer")));
+                }
+                con.Close();
+                return ListPS;
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
+    }
+}
--- Personenverwaltung/Controllers/controller.cs
+++ Personenverwaltung/Controllers/controller.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using Personenverwaltung.Models;
 
@@ -9,6 +11,33 @@ namespace Personenverwaltung.Controllers
     public class controller
     {
         private List<Person> _ListPS;
-        
+        public List<Person> ListPS { get => _ListPS; set => _ListPS = value; }
+
+        public controller()
+        {
+            this.ListPS = new List<Person>();
+            ListPS.Add(new Fussballspieler());
+        }
+
+        public List<Person> GetAllPersons()
+        {
+            string json = HTTPRequest("API/Person");
+
+            return ListPS;
+        }
+
+        public string HTTPRequest(string URI, string Port = "44328")
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://localhost:" + Port + "/" + URI);
+            request.Method = "GET";
+            request.MaximumAutomaticRedirections = 4;
+            request.MaximumResponseHeadersLength = 4;
+            request.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream recieve = response.GetResponseStream();
+            StreamReader reader = new StreamReader(recieve);
+            string retResponse = reader.ReadToEnd();
+            return retResponse;
+        }
     }
 }
